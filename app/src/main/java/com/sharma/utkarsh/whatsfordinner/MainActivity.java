0407cc4f50b9a911e73825aa8b8f0c5bd5b07c2d
Utkarsh Sharma <utkarsh7sharma@gmail.com>
2017-10-04 12:25:15 -0700

    Draft 1

--- app/src/main/java/com/sharma/utkarsh/whatsfordinner/MainActivity.java
+++ app/src/main/java/com/sharma/utkarsh/whatsfordinner/MainActivity.java
@@ -2,12 +2,33 @@ package com.sharma.utkarsh.whatsfordinner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    final Button btn1=(Button)findViewById(R.id.button);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView im = (ImageView)findViewById(R.id.imageView);
+        im.setScaleType(ImageView.ScaleType.FIT_XY);
+
+        ImageButton menu = (ImageButton)findViewById(R.id.menuBtn);
+
+
     }
-}
+
+    protected void onMenuClick(View view){
+
+    }
+
+
+
+     }
+
+
